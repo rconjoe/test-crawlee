910efd6d61eff1be84e179c8890ac20e78c27151
rconjoe <root@trog.codes>
2025-05-17 00:10:15 -0700

    here i understand the implicit faculties of a crawler class

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { RequestQueue, CheerioCrawler } from "crawlee";
 
 // request queue is list of shit you wanna scrape, whereas
-// request handler is what you want to do on each one?
+// request handler is what you want to do on each one.
+//
+// There is an implicit request queue in each crawler,
+// and you can implicitly add requests to it with the first parameter of crawler.run:
 
-const requestQueue = await RequestQueue.open();
-await requestQueue.addRequest({ url: 'https://crawlee.dev' });
 
 const crawler = new CheerioCrawler({
-    requestQueue,
     async requestHandler({ $, request }) {
         const title = $('title').text();
-        console.log(`The title of "${request.url}" is: ${title}`)
+        console.log(`The title of "${request.url}" is: ${title}.`)
     }
-})
+});
 
-await crawler.run();
+await crawler.run(['https://crawlee.dev']);
